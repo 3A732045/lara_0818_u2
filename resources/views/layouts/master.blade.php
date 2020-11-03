9844c4e15a516e840b3a1b5bb1da6d13d602be36
李沐恩 <jesssezzxxcc@gmail.com>
2020-11-03 18:20:21 +0800

    練習2-3 建立共用區域 新增partials資料夾，新增navigation.blade.php，編輯 navigation.blade.php，編輯master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,8 @@
 <title>@yield('title')|MyBlog</title>
 </head>
     <body>
-        <div class="container">
+    @include('layouts.partials.navigation')
+    <div class="container">
             @yield('content')
         </div>
     </body>
